a801250b0c166d629be44504d41b37a5c85ffd91
yrk9 <s243151@stn.nagaokaut.ac.jp>
2026-04-20 17:42:03 +0900

    fix: デプロイ用に修正

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,12 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/dbname"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
